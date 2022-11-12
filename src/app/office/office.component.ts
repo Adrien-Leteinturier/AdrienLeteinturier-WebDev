b481d571be4a2086308cfd6d8bbb60e9d34d2a0a
Leteinturier <adrienleteinturier@yahoo.fr>
2022-11-12 01:30:46 +0100

    indentation

--- src/app/office/office.component.ts
+++ src/app/office/office.component.ts
@@ -1,31 +1,31 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
-import {AuthService} from '../providers/auth.service';
-import {Observable} from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../providers/auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
-
+  
   selector: 'office',
   templateUrl: './office.component.html',
   styleUrls: ['./office.component.css']
-
+  
 })
 
 export class OfficeComponent implements OnInit {
-
+  
   user: Observable<any>;
   pseudo: string;
   email: string;
   photoUrl: string;
-
+  
   constructor(private authService: AuthService, private router: Router) {
     this.user = this.authService.afAuth.authState;
     this.user.subscribe((auth) => {
-        this.addValueAuth(auth);
-      }
+      this.addValueAuth(auth);
+    }
     );
   };
-
+  
   addValueAuth(auth) {
     if (auth) {
       this.pseudo = auth.displayName;
@@ -33,14 +33,14 @@ export class OfficeComponent implements OnInit {
       this.photoUrl = auth.photoURL;
     }
   }
-
+  
   ngOnInit() {
   }
-
-
+  
+  
   logout() {
     this.authService.logout();
     window.location.href = '/';
-
+    
   }
 }
